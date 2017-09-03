8c40240f5089932d5110663d05a8597589c61682
hecgarri <hectorgarridohenriquez@gmail.com>
2017-09-03 16:34:39 -0300

    Actualización de los datos. Ahora vigentes hasta agosto de 2017

--- weekly_palta.R
+++ weekly_palta.R
@@ -1,32 +1,17 @@
 rm(list=ls())
-setwd("/home/hector/GoogleDrivePersonal/Master/Tesis/datos/semanales/palta")
+setwd("~/GoogleDrivePersonal/Master/Tesis/datos/semanales/palta")
 getwd()
 
-palta_2008 = read.csv("2008_consumidor.csv", sep=",")
-palta_2009 = read.csv("2009_consumidor.csv", sep=",")
-palta_2010 = read.csv("2010_consumidor.csv", sep=",")
-palta_2011 = read.csv("2011_consumidor.csv", sep=",")
-palta_2012 = read.csv("2012_consumidor.csv", sep=",")
-palta_2013 = read.csv("2013_consumidor.csv", sep=",")
-palta_2014 = read.csv("2014_consumidor.csv", sep=",")
-palta_2015 = read.csv("2015_consumidor.csv", sep=",")
-palta_2016 = read.csv("2016_consumidor.csv", sep=",")
+if (!require(data.table)) install.packages("data.table")
+if (!require(tidyverse)) install.packages("tidyverse")
+
+paltas = list.files(pattern="consumidor.csv$")
+
+palta_cons = lapply(paltas, function(x) fread(x)) %>% rbindlist()
 
 
-palta_cons = rbind(palta_2008, palta_2009, palta_2010, 
-                    palta_2011, palta_2012, palta_2013, 
-                    palta_2014, palta_2015, palta_2016)
-
-#' Por el momento, utilizaremos solamente el tomate larga vida y la lechuga escarola para el análisis 
-
-
-## ----Creación de las serie de precios al consumidor
-library(magrittr)
-library(dplyr)
-library(tidyr)
-
-p_super_palta = filter(palta_cons, Variedad=="Hass " & Calidad=="Primera " & 
-                       Tipo.punto.monitoreo=="Supermercado ") %>% 
+p_super_palta = filter(palta_cons, Variedad=="Hass" & Calidad=="Primera" & 
+                       `Tipo punto monitoreo`=="Supermercado") %>% 
   `colnames<-` (c("week", "start", "end", "place","product",
                   "variety","quality","unity","min","max","price")) %>% 
   mutate(price=price/1.19,start =gsub("/","-",start))
@@ -34,12 +19,9 @@ p_super_palta = filter(palta_cons, Variedad=="Hass " & Calidad=="Primera " &
 
 #generaré una secuencia que tiene todas las semanas para luego comparar con mi secuencia incompleta
 
-ts = seq.POSIXt(as.POSIXct("2008-03-17", '%y%m%d'), as.POSIXct("2016-12-26", '%y%m%d'), by="week")
+ts = seq.POSIXt(as.POSIXct("2008-03-17", '%y%m%d'), as.POSIXct("2017-08-28", '%y%m%d'), by="week")
 
 df <- data.frame(start=ts)
-
-
-
 
 p_super_palta = separate(data=p_super_palta,col = "start",
                        into = c("day", "month", "year"),sep = "\\-",extra="drop")
@@ -69,12 +51,15 @@ mayor = filter(mayor, Variedad=="Hass " & Calidad=="Primera ") %>%
 
 mayor$Unidad.de.comercialización. = recode(as.character(mayor$Unidad.de.comercialización.),
         "$/bandeja 10 kilos "=10, 
-       "$/bandeja 4 kilos embalada " = 4, "$/caja 12 kilos " = 12, 
+       "$/bandeja 4 kilos embalada " = 4,"$/bandeja 8 kilos "=8,
+       "$/caja 12 kilos " = 12, 
        "$/kilo (en bins de 400 kilos) " = 400, "$/kilo (en bins de 450 kilos) " = 450,
-       "$/kilo (en caja de 15 kilos) " = 15, "$/kilo (en caja de 17 kilos) " = 17)
+       "$/kilo (en caja de 15 kilos) " = 15, "$/kilo (en caja de 17 kilos) " = 17, 
+       "$/kilo (en caja de 20 kilos) " = 20, "$/kilo (en caja de 8 kilos ) " = 8)
 
 mayor$precio_definitivo = ifelse(mayor$Unidad.de.comercialización.==10 | 
                                    mayor$Unidad.de.comercialización.==4 |
+                                   mayor$Unidad.de.comercialización.==8 |
                                    mayor$Unidad.de.comercialización.== 12, 
                                  mayor$Precio.promedio./mayor$Unidad.de.comercialización., 
                                  mayor$Precio.promedio.)
